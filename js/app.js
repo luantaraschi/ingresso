9e3b6f1496cf734035305b16552ab1684c4da81e
luantaraschi <luantaraschi@gmail.com>
2024-10-10 15:28:02 -0300

    outras opcoes de compra agora sao funcionais

--- js/app.js
+++ js/app.js
@@ -4,6 +4,10 @@ function comprar() {
 
     if(tipo.value == 'pista') {
         comprarPista(qtd);
+    } else if (tipo.value == 'superior') {
+        comprarSuperior(qtd);
+    } else {
+        comprarInferior(qtd);
     }
 }
 
@@ -17,3 +21,24 @@ function comprarPista(qtd) {
         alert('Compra realizada com sucesso');
     }
 }
+function comprarSuperior(qtd) {
+    let qtdSuperior = parseInt(document.getElementById('qtd-superior').textContent);
+    if (qtd > qtdSuperior) {
+        alert('Quantidade de ingressos não disponível para cadeira superior.');
+    } else {
+        qtdSuperior = qtdSuperior - qtd;
+        document.getElementById('qtd-superior').textContent = qtdSuperior;
+        alert('Compra realizada com sucesso');
+    }
+}
+function comprarInferior(qtd) {
+    let qtdInferior = parseInt(document.getElementById('qtd-inferior').textContent);
+    if (qtd > qtdInferior) {
+        alert('Quantidade de ingressos não disponível para cadeira inferior.');
+    } else {
+        qtdInferior = qtdInferior - qtd;
+        document.getElementById('qtd-inferior').textContent = qtdInferior;
+        alert('Compra realizada com sucesso');
+    }
+}
+
